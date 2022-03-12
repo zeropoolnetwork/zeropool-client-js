080bf0d046dcfef4b54ce91135d1637995ebd7a9
Dmitry Vdovin <voidxnull@gmail.com>
2022-03-13 03:14:17 +0400

    Fix state sync

--- src/client.ts
+++ src/client.ts
@@ -244,7 +244,7 @@ export class ZeropoolClient {
 
         const memo = tx.slice(64); // Skip commitment
         const hashes = parseHashes(memo);
-        this.cacheShieldedTx(tokenAddress, tx, hashes, i);
+        this.cacheShieldedTx(tokenAddress, memo, hashes, i);
       }
     }
   }
