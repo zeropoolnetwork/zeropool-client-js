5f652c21e538a2ee0980509931f5d827ca059b9f
Dmitry Vdovin <voidxnull@gmail.com>
2022-03-15 21:41:52 +0400

    Fix truncateHexPrefix

--- src/utils.ts
+++ src/utils.ts
@@ -176,10 +176,12 @@ export function toTwosComplementHex(num: bigint, numBytes: number): string {
   return padLeft(hex, numBytes * 2);
 }
 
-export function toCompactSignature(signature: string) {
-  let v = signature.substr(130, 2);
+export function toCompactSignature(signature: string): string {
+  signature = truncateHexPrefix(signature);
+
+  let v = signature.substr(128, 2);
   if (v == "1c") {
-    return `${signature.slice(0, 66)}${(parseInt(signature[66], 16) | 8).toString(16)}${signature.slice(67, 130)}`;
+    return `${signature.slice(0, 64)}${(parseInt(signature[64], 16) | 8).toString(16)}${signature.slice(65, 128)}`;
   } else if (v != "1b") {
     throw ("invalid signature: v should be 27 or 28");
   }
@@ -187,9 +189,11 @@ export function toCompactSignature(signature: string) {
   return signature;
 }
 
-export function toCanonicalSignature(signature: string) {
+export function toCanonicalSignature(signature: string): string {
+  signature = truncateHexPrefix(signature);
+
   let v = "1c";
-  if (parseInt(signature[66], 16) > 7) {
+  if (parseInt(signature[64], 16) > 7) {
     v = "1e";
   }
   return signature + v;
