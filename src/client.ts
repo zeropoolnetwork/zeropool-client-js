36081802e2a011c8f23c8b8769ce2eb66987c7e6
Alexander Filippov <aleksander.fill@gmail.com>
2022-05-21 16:33:52 +0300

    Fix package.json, remove unnecessary imports

--- src/client.ts
+++ src/client.ts
@@ -1,13 +1,13 @@
-import { Note, Output, Proof } from 'libzeropool-rs-wasm-web';
+import { Output, Proof } from 'libzeropool-rs-wasm-web';
 
 import { SnarkParams, Tokens } from './config';
 import { hexToBuf, toCompactSignature, truncateHexPrefix } from './utils';
 import { ZeroPoolState } from './state';
-import { parseHashes, ShieldedTx, TxType } from './tx';
+import { TxType } from './tx';
 import { NetworkBackend } from './networks/network';
 import { CONSTANTS } from './constants';
-import { HistoryTransactionType, HistoryRecord, HistoryRecordIdx, HistoryStorage, DecryptedMemo } from './history'
 import { zp } from './zp';
+import { HistoryRecord } from './history'
 import { IndexedTx } from 'libzkbob-rs-wasm-web';
 
 export interface RelayerInfo {
