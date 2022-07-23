618aee4523e1a5e74c42dfdeba58efbfd1a721ac
Dmitry Vdovin <voidxnull@gmail.com>
2022-07-23 18:19:23 +0400

    Simplify init function
    Should work out of the box with Webpack 5 or parcel 2 with no extra
    configuration.

--- src/index.ts
+++ src/index.ts
@@ -10,22 +10,24 @@ export { TxType } from './tx';
 
 export { HistoryRecord, HistoryTransactionType } from './history'
 
+const WASM_PATH = new URL('libzkbob-rs-wasm-web/libzeropool_rs_wasm_bg.wasm', import.meta.url).href;
+
 export class ZkBobLibState {
   public fileCache: FileCache;
   public worker: any;
   public snarkParams: SnarkParams;
 }
 
-export async function init(wasmPath: string, workerPath: string, snarkParams: SnarkConfigParams): Promise<ZkBobLibState> {
+export async function init(snarkParams: SnarkConfigParams): Promise<ZkBobLibState> {
   const fileCache = await FileCache.init();
 
-  const worker: any = wrap(new Worker(workerPath));
-  await worker.initWasm(wasmPath, {
+  const worker: any = wrap(new Worker(new URL('./worker.ts', import.meta.url), { type: 'module' }));
+  await worker.initWasm(WASM_PATH, {
     txParams: snarkParams.transferParamsUrl,
     treeParams: snarkParams.treeParamsUrl,
   });
 
-  await initWasm(wasmPath);
+  await initWasm(WASM_PATH);
   const transferVk = await (await fetch(snarkParams.transferVkUrl)).json();
   const treeVk = await (await fetch(snarkParams.treeVkUrl)).json();
 
