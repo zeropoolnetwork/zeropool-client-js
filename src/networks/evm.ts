c25f035a2578621dafe9f9518ead3a8faabecd2b
Evgen <evgen2k7@yandex.ru>
2022-09-04 20:49:19 +0300

    Check depositer address for limits for approve-allowance deposit

--- src/networks/evm.ts
+++ src/networks/evm.ts
@@ -3,21 +3,16 @@ import { AbiItem } from 'web3-utils';
 import { Contract } from 'web3-eth-contract'
 import { NetworkBackend } from './network';
 
-const EthDater = require('ethereum-block-by-date');
-
 export class EvmNetwork implements NetworkBackend {
     contract: Contract;
     token: Contract;
     rpcUrl: string;
     web3: Web3;
-    dater: any;
 
     constructor(rpcUrl: string) {
         this.rpcUrl = rpcUrl;
 
         this.web3 = new Web3(rpcUrl);
-
-        this.dater = new EthDater(this.web3);
 
         const abi: AbiItem[] = [
             {
@@ -116,6 +111,10 @@ export class EvmNetwork implements NetworkBackend {
         this.token = new this.web3.eth.Contract(abiTokenJson) as Contract;
     }
 
+    public async getChainId(): Promise<number> {
+        return await this.web3.eth.getChainId();
+    }
+
     public async getDenominator(contractAddress: string): Promise<bigint> {
         this.contract.options.address = contractAddress;
         return BigInt(await this.contract.methods.denominator().call());
@@ -131,32 +130,6 @@ export class EvmNetwork implements NetworkBackend {
 
     getRpcUrl(): string {
         return this.rpcUrl;
-    }
-
-    // result in wei
-    public async tokenTransferedAmount(tokenAddress: string, from: string, to: string): Promise<bigint> {
-        this.token.options.address = tokenAddress;
-
-        const startOfDayUTC = new Date();
-        startOfDayUTC.setUTCHours(0, 0, 0, 0);
-        let dateBlock = await this.dater.getDate(startOfDayUTC, true, false);
-        const fromBlock = dateBlock.block;
-        const toBlock =await this.web3.eth.getBlockNumber();
-
-        let totalTokensTranferred = BigInt(0);
-        const pastEvents = await this.token.getPastEvents('Transfer', {
-            'filter': { from, to },
-            'fromBlock': fromBlock,
-            'toBlock': toBlock,
-        });
-
-        for (let pastEvent of pastEvents) {
-            totalTokensTranferred += BigInt(pastEvent.returnValues.value);
-        }
-        
-        console.log(`Fetched transfers from block ${fromBlock}, to block ${toBlock}: ${totalTokensTranferred} wei in ${pastEvents.length} transactions`);
-
-        return totalTokensTranferred;
     }
 
     public async poolLimits(contractAddress: string, address: string | undefined): Promise<any> {
